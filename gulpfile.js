31b26c01be020e095f15a74e993738a90a6238d8
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-12 12:45:53 +1000

    local web server
    - gulp connect
    - index page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require( 'gulp' );
+var connect = require( 'gulp-connect' );
 
-gulp.task( 'default', function() {
-	// place code for your default task here
+
+// local server
+gulp.task( 'connect', function() {
+	connect.server({ root: 'app/' });
 });
+
+
+// default task
+gulp.task( 'default', [ 'connect' ]);
