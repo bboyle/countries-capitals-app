1a098fad8d257b896fe913042a0161597d624080
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-12 16:24:34 +1000

    add countries view
    - initial css
    - new view for countries (dummy content)

--- app/countriesCapitalsApp.js
+++ app/countriesCapitalsApp.js
@@ -2,5 +2,6 @@ angular.module( 'countriesCapitalsApp', [ 'ngRoute' ])
 .config([ '$routeProvider', function( $routeProvider ) {
 	$routeProvider
 	.when( '/', { templateUrl: 'views/index.html' })
+	.when( '/countries', { templateUrl: 'views/countries/countries.html' })
 	.otherwise({ redirectTo : '/' });
 }]);
