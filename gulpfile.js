4310d02b2736612d87abeace220a2cce1827d82e
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-12 13:28:13 +1000

    linting
    - lint gulp file before running server
    - gulp test
    - npm test

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require( 'gulp' );
 var connect = require( 'gulp-connect' );
+var jshint = require( 'gulp-jshint' );
+
+
+// jshint
+gulp.task( 'lint', function() {
+	return gulp.src( '*.js' )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( 'default' ))
+		.pipe( jshint.reporter( 'fail' ));
+});
 
 
 // local server
-gulp.task( 'connect', function() {
+gulp.task( 'connect', [ 'test' ], function() {
 	connect.server({ root: 'app/' });
 });
 
 
 // default task
-gulp.task( 'default', [ 'connect' ]);
+gulp.task( 'test', [ 'lint' ] );
+gulp.task( 'default', [ 'connect' ] );
