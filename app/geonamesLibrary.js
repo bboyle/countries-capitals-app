ed35ba82c7514f57895eb4a67c61c82a02c35136
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-15 14:37:36 +1000

    wip: country list service
    create service for querying geonames API to get list of countries

---
+++ app/geonamesLibrary.js
@@ -0,0 +1,31 @@
+angular.module( 'geonamesLibrary', [])
+.constant( 'LIST_COUNTRIES_URL', 'http://api.geonames.org/countryInfo?username={{ username }}&type=JSON')
+.constant( 'GEONAMES_USER', 'bboyle')
+
+
+// geonames API requests
+.factory( 'geonamesRequest', [ '$http', '$interpolate', '$q', 'GEONAMES_USER',
+                     function(  $http,   $interpolate,   $q,   GEONAMES_USER ) {
+	return function( url ) {
+		var defer = $q.defer();
+		url = $interpolate( url, {
+			username: GEONAMES_USER
+		});
+		$http.get( API_URL )
+		.success(function( data ) {
+			defer.resolve( data );
+		});
+		return defer.promise;
+}
+}])
+
+
+// get list of countries
+// http://www.geonames.org/export/web-services.html#countryInfo
+.factory( 'listCountries', [ 'geonamesRequest', '$interpolate', 'LIST_COUNTRIES_URL',
+                   function(  geonamesRequest,   $interpolate,   LIST_COUNTRIES_URL ) {
+	return function() {
+		return owmRequest( LIST_COUNTRIES_URL );
+	}
+}])
+;
