d032f106308d407731e79ae908c85f1d2f078332
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-18 18:46:36 +1000

    whitespace
    readability improvements

--- app/geonamesLibrary.js
+++ app/geonamesLibrary.js
@@ -6,15 +6,16 @@ angular.module( 'geonamesLibrary', [] )
 // geonames API requests
 .factory( 'geonamesRequest', [ '$http', '$interpolate', '$q', 'GEONAMES_USER',
                      function(  $http,   $interpolate,   $q,   GEONAMES_USER ) {
+
 	return function( url ) {
 		var defer = $q.defer();
-		url = $interpolate( url, {
-			username: GEONAMES_USER
-		});
+		url = $interpolate( url, { username: GEONAMES_USER });
+
 		$http.get( API_URL )
 		.success(function( data ) {
 			defer.resolve( data );
 		});
+
 		return defer.promise;
 }
 }])
@@ -24,6 +25,7 @@ angular.module( 'geonamesLibrary', [] )
 // http://www.geonames.org/export/web-services.html#countryInfo
 .factory( 'listCountries', [ 'geonamesRequest', '$interpolate', 'LIST_COUNTRIES_URL',
                    function(  geonamesRequest,   $interpolate,   LIST_COUNTRIES_URL ) {
+
 	return function() {
 		return geonamesRequest( LIST_COUNTRIES_URL );
 	}
