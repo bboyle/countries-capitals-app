8952f9af43d7ebc47d85467ce03f368d34cb84a6
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-27 19:22:22 +1000

    add loading placeholder
    - display `progress` when loading
    - watch routing events

--- app/countriesCapitalsApp.js
+++ app/countriesCapitalsApp.js
@@ -5,4 +5,23 @@ angular.module( 'countriesCapitalsApp', [ 'countryCapitalsAppViews', 'ngRoute', 
 	.when( '/', { templateUrl: 'views/index.html' })
 	.otherwise({ redirectTo : '/' });
 
+}])
+
+.run([ '$rootScope', function( $rootScope ) {
+	// $rootScope.$on( '$routeChangeError', function() {
+	// 	$rootScope.isLoading = false;
+	// 	$location.path( '/error' );
+	// });
+
+	$rootScope.$on( '$routeChangeStart', function() {
+		$rootScope.isLoading = true;
+		$rootScope.loadingPercent = 10;
+		console.log( 'routeChangeStart', $rootScope.isLoading, $rootScope.loadingPercent );
+	});
+	$rootScope.$on( '$routeChangeSuccess', function() {
+		$rootScope.isLoading = false;
+		$rootScope.loadingPercent = 100;
+		console.log( 'routeChangeSuccess', $rootScope.isLoading, $rootScope.loadingPercent );
+	});
+
 }]);
