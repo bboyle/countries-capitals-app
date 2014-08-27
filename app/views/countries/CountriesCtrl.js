80ba5378450086b7887457092ed1ee3ff26fc6b9
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-27 21:57:34 +1000

    simple pagination
    - limit to 20 results per page
    - display result number in table
    - back/next buttons to move through pages

--- app/views/countries/CountriesCtrl.js
+++ app/views/countries/CountriesCtrl.js
@@ -17,12 +17,46 @@ viewsModule.config([ '$routeProvider', function( $routeProvider ) {
 viewsModule.controller( 'CountriesCtrl', [ 'countriesList', '$timeout',
 function(                                   countriesList,   $timeout ) {
 
-   	var vm = this;
-   	vm.countries = [];
+	var vm = this;
 
-   	// delay countries data (required to trigger ngrepeat animation when view loads)
-   	$timeout(function() {
-		vm.countries = countriesList.geonames;
-   	}, 1 );
+	vm.allCountries = countriesList.geonames;
+
+	vm.pagination = {
+		start: 0,
+		page: 20, // results per page
+		showPrevious: false,
+		showNext: false,
+
+		next: function() {
+			if ( this.start + this.page < vm.allCountries.length ) {
+				this.start += this.page;
+			}
+			vm.refresh();
+		},
+		previous: function() {
+			if ( this.start > 0 ) {
+				this.start -= this.page;
+			}
+			vm.refresh();
+		}
+	}
+
+	vm.refresh = function() {
+		if ( this.pagination.start + this.pagination.page < this.allCountries.length ) {
+			this.pagination.showNext = true;
+		}
+		if ( this.pagination.start > 0 ) {
+			this.pagination.showPrevious = true;
+		}
+		this.countries = this.allCountries.slice( this.pagination.start, this.pagination.start + this.pagination.page );
+	};
+
+
+	vm.countries = [];
+
+	// delay countries data (required to trigger ngrepeat animation when view loads)
+	$timeout(function() {
+		vm.refresh();
+	}, 1 );
 
 }]);
