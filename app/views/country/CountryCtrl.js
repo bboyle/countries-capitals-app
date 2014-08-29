10d3e63356cf6085fbc93812b44ea19f529a3f35
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-29 15:56:00 +1000

    add leaflet map
    - setup map bounds based on country
    - parse lat/long as floats in capital info
    - show marker for capital

--- app/views/country/CountryCtrl.js
+++ app/views/country/CountryCtrl.js
@@ -27,8 +27,47 @@ viewsModule.controller( 'CountryCtrl', [ 'country', 'countryCapitalsModel', '$lo
 	var vm = this;
 	vm.country = country;
 
+	// http://leaflet-extras.github.io/leaflet-providers/preview/
+	var Esri_NatGeoWorldMap = {
+		url: 'http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
+		options: {
+			attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+		}
+	};
+
+	// map UI
+	// http://tombatossals.github.io/angular-leaflet-directive/#!/examples/simple-map
+	vm.map = {
+		tiles: Esri_NatGeoWorldMap,
+		maxbounds: {
+			northEast: {
+				lat: country.north,
+				lng: country.east
+			},
+			southWest: {
+				lat: country.south,
+				lng: country.west
+			}
+		},
+		markers: {},
+		// center: {
+		// 	lat: country.north - (( country.north - country.south ) / 2 ),
+		// 	lng: country.east - (( country.east - country.west ) / 2 ),
+		// 	zoom: 4
+		// }
+	};
+
 	// lazy load capital details
-	countryCapitalsModel.extendCountryInfoWithCapital( country );
+	countryCapitalsModel.extendCountryInfoWithCapital( country ).then(function() {
+		// place marker for capital
+		var capital = vm.country.capital;
+
+		vm.map.markers.capital = {
+			lat: capital.lat,
+			lng: capital.lng,
+			message: capital.name
+		};
+	});
 
 	// lazy load neighbours
 	countryCapitalsModel.getCountryNeighbours( country ).then(function( data ) {
