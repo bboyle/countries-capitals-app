a14d7119ead56413fc9a24f276bc629631ebd620
Ben Boyle <benjamins.boyle@gmail.com>
2014-09-08 17:26:34 +1000

    usemin configuration
    gulp config and names of assets in html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
+var BUILD_FOLDER = 'build/';
+
 var gulp = require( 'gulp' );
 var connect = require( 'gulp-connect' );
 var jshint = require( 'gulp-jshint' );
-
+var usemin = require( 'gulp-usemin' );
+var uglify = require( 'gulp-uglify' );
+// var minifyHtml = require( 'gulp-minify-html' );
+var minifyCss = require( 'gulp-minify-css' );
+var rev = require( 'gulp-rev' );
 
 // jshint
 gulp.task( 'lint', function() {
@@ -12,12 +18,35 @@ gulp.task( 'lint', function() {
 });
 
 
+// copy html to build
+gulp.task( 'copy-html-files', function() {
+	return gulp.src([
+		'./app/**/*.html',
+		'./app/image/*.jpg'
+	], { base: './app' })
+		.pipe( gulp.dest( BUILD_FOLDER ));
+});
+
+
+// usemin: css and js concat and compression
+gulp.task( 'usemin', function() {
+	return gulp.src( './app/*.html' )
+		.pipe(usemin({
+			css: [ minifyCss(), 'concat' ],
+			// html: [ minifyHtml({ empty: true }) ],
+			js: [ uglify(), rev() ]
+		}))
+		.pipe( gulp.dest( BUILD_FOLDER ));
+});
+
+
 // local server
 gulp.task( 'connect', [ 'test' ], function() {
-	connect.server({ root: 'app/' });
+	connect.server({ root: 'build/' });
 });
 
 
 // default task
-gulp.task( 'test', [ 'lint' ] );
-gulp.task( 'default', [ 'connect' ] );
+gulp.task( 'build', [ 'copy-html-files', 'usemin' ]);
+gulp.task( 'test', [ 'lint' ]);
+gulp.task( 'default', [ 'connect' ]);
